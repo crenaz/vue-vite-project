8c7e72bfdfebdb4513a8ce77b7a9c42d61bb4009
crenaz <crenaz@gmail.com>
2022-11-23 20:09:43 -0400

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h1>This is the about page but as can see, there is really nothing to see here.</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
